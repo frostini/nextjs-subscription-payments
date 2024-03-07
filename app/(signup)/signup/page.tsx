34a854b8bb661a611cfe84878b1e1379b480d5ac
Marlon j. Frausto <marlonjfrausto@gmail.com>
2024-03-07 18:51:13 +0000

    WIP. Simple server action for signup form submission with multiselect component. Will proceed to adding validations and submission state

--- app/(signup)/signup/page.tsx
+++ app/(signup)/signup/page.tsx
@@ -15,6 +15,33 @@
 import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 export default function Example() {
+
+  async function createSample(formData: FormData) {
+    'use server'
+
+    const rawFormData = {
+      firstName: formData.get('firstName'),
+      lastName: formData.get('lastName'),
+      phoneNumber: formData.get('phoneNumber'),
+      companyName: formData.get('companyName'),
+      promoCode: formData.get('promoCode'),
+      location: formData.getAll('location')
+    }
+
+    console.log(`rawFormData:`, rawFormData)
+    return rawFormData;
+  // example for a lot of different fields should use the formData.fromEntries() method. 
+  }
+
+  const tradeOptions = [
+    "electric",
+    "plumbing",
+    "general construction",
+    "framing",
+    "other",
+    "demolition",
+  ]
+
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -86,7 +113,7 @@ export default function Example() {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+        <form action={createSample}  className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
@@ -96,7 +123,7 @@ export default function Example() {
                 <div className="mt-2.5">
                   <input
                     type="text"
-                    name="first-name"
+                    name="firstName"
                     id="first-name"
                     autoComplete="given-name"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -110,7 +137,7 @@ export default function Example() {
                 <div className="mt-2.5">
                   <input
                     type="text"
-                    name="last-name"
+                    name="lastName"
                     id="last-name"
                     autoComplete="family-name"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -138,25 +165,60 @@ export default function Example() {
                 <div className="mt-2.5">
                   <input
                     type="tel"
-                    name="phone-number"
+                    maxLength={10}
+                    minLength={10}
+                    name="phoneNumber"
                     id="phone-number"
                     autoComplete="tel"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="company-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Company Name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="companyName"
+                    id="company-name"
+                    autoComplete="organization"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-                  Message
+                <label htmlFor="company-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Promo Code
                 </label>
                 <div className="mt-2.5">
-                  <textarea
-                    name="message"
-                    id="message"
-                    rows={4}
+                  <input
+                    type="text"
+                    name="promoCode"
+                    id="promo-code"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={''}
                   />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="location" className="block text-sm font-medium leading-6 text-gray-900">
+                  Location
+                </label>
+                <div className="mt-2.5">
+                  <select
+                    id="location"
+                    name="location"
+                    className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    // defaultValue={["Canada"]}
+                    multiple={true}
+                  >
+                    {tradeOptions.map((option, index) => (
+                      <option key={index} value={option}>{option}</option>)
+                    )}
+                  </select>
                 </div>
               </div>
             </div>
